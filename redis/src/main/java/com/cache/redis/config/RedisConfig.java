72e09ce6d167c95a94c5e23ec50373223380fe27
노경준 <developerjun0615@gmail.com>
2022-06-08 23:22:43 +0900

    Update RedisConfig Redis 설정 변경

--- redis/src/main/java/com/cache/redis/config/RedisConfig.java
+++ redis/src/main/java/com/cache/redis/config/RedisConfig.java
@@ -1,27 +1,12 @@
 package com.cache.redis.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-
-    /**
-     * yml 파일에 등록되어있는 커넥션 정보를 변수에 주입
-     */
-    @Value("${spring.redis.port}")
-    public int port;
-
-    @Value("${spring.redis.host}")
-    public String host;
 
     /**
      * Redis 를 연결하기 위한 팩토리 메소드 Bean
@@ -30,25 +15,7 @@ public class RedisConfig {
      */
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        return new LettuceConnectionFactory();
     }
 
-    /**
-     * redisConnectionFactory 를 이용하여 Redis 와 연결한 뒤 캐싱 기능을 연시킬 Bean
-     */
-    @Bean
-    public RedisCacheManager redisCacheManager() {
-        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-                .serializeKeysWith(
-                        RedisSerializationContext.SerializationPair.fromSerializer(
-                                new StringRedisSerializer()))
-                .serializeValuesWith(
-                        RedisSerializationContext.SerializationPair.fromSerializer(
-                                new GenericJackson2JsonRedisSerializer()));
-
-        return RedisCacheManager.RedisCacheManagerBuilder
-                .fromConnectionFactory(redisConnectionFactory())
-                .cacheDefaults(redisCacheConfiguration)
-                .build();
-    }
 }
