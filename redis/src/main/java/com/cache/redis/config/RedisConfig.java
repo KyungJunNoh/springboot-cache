2953e576c347fb6e9f1ec42ce06673bac5080338
노경준 <developerjun0615@gmail.com>
2022-06-09 23:59:18 +0900

    Update RedisConfig

--- redis/src/main/java/com/cache/redis/config/RedisConfig.java
+++ redis/src/main/java/com/cache/redis/config/RedisConfig.java
@@ -1,21 +1,61 @@
 package com.cache.redis.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+/**
+ * Redis 를 사용하기 위한 Config 클래스
+ */
+@EnableCaching
+@RequiredArgsConstructor
 @Configuration
 public class RedisConfig {
 
+    private final RedisConnectionFactory connectionFactory;
+
     /**
-     * Redis 를 연결하기 위한 팩토리 메소드 Bean
-     * RedisStandaloneConfiguration 클래스를 이용하여 redis 설정정보를 기반으로 연결을 함
-     * @apiNote https://docs.spring.io/spring-data/redis/docs/current/api/org/springframework/data/redis/connection/RedisStandaloneConfiguration.html
+     * redis 에서 값을 저장하고 가져오는 과정에서 LocalDateTime 이나 LocalDateTime 형식은 역직렬화하는 과정에서 에러가 나기 때문에 ObjectMapper를 Bean 으로 등록
+     * @return objectMapper
      */
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // timestamp 형식 안따르도록 설정
+        objectMapper.registerModules(new JavaTimeModule(), new Jdk8Module()); // LocalDateTime 매핑을 위해 모듈 활성화
+
+        return objectMapper;
+    }
+
+    /**
+     * spring data redis Cache 에서 지원하는 RedisCacheConfig 와 Redis 를 Connection 하는 메소드
+     * @return redisCacheManager
+     */
+    @Bean
+    public CacheManager redisCacheManager() {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+        RedisCacheManager redisCacheManager = RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(connectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+
+        return redisCacheManager;
     }
 
 }
+
