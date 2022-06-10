43ba8e62b86e828a4b03ea166a073754226a2c37
노경준 <developerjun0615@gmail.com>
2022-06-10 14:42:45 +0900

    Update MenuService 주석 추가 + 코드 약간 수정

--- redis/src/main/java/com/cache/redis/service/MenuService.java
+++ redis/src/main/java/com/cache/redis/service/MenuService.java
@@ -5,7 +5,6 @@ import com.cache.redis.repository.MenuRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,12 +14,22 @@ public class MenuService {
 
     private final MenuRepository menuRepository;
 
-    @Cacheable(value = "menu")
+    /**
+     * 메뉴를 전체 조회하는 서비스 로직
+     * 저장될때 "menu(CacheName)::SimpleKey [](Key)" 형식으로 저장
+     * @return menu
+     */
+    @Cacheable(cacheNames = "menu")
     public List<Menu> getAllMenu() {
         return menuRepository.findAll();
     }
 
-    @Cacheable(key = "#id", value = "menu")
+    /**
+     * 메뉴를 단일 조회하는 서비스 로직
+     * 저장될때 "menu(CacheName)::num(Key)" 형식으로 저장
+     * @return menu
+     */
+    @Cacheable(key = "#id", cacheNames = "menu")
     public Menu getOneMenu(Long id) {
         return menuRepository.findById(id).orElseThrow();
     }
